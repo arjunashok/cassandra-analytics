c2505d7c7d2b176d1256ecb34cf9527cb729fbee
Francisco Guerrero <frankgh@apache.org>
2023-11-02 16:13:35 -0700

    Provide a fast resolver to speed up tests that might block on reverse DNS resolution

---
+++ cassandra-analytics-integration-framework/src/test/java/org/apache/cassandra/sidecar/testing/FastDnsResolverTest.java
@@ -0,0 +1,28 @@
+package org.apache.cassandra.sidecar.testing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FastDnsResolverTest
+{
+    @Test
+    void testResolve()
+    {
+        IntegrationTestBase.FastDnsResolver resolver = new IntegrationTestBase.FastDnsResolver();
+        assertThat(resolver.resolve("localhost")).isEqualTo("127.0.0.1");
+        assertThat(resolver.resolve("localhost2")).isEqualTo("127.0.0.2");
+        assertThat(resolver.resolve("localhost20")).isEqualTo("127.0.0.20");
+        assertThat(resolver.resolve("127.0.0.2")).isEqualTo("127.0.0.2");
+    }
+
+    @Test
+    void testReverseResolve()
+    {
+        IntegrationTestBase.FastDnsResolver resolver = new IntegrationTestBase.FastDnsResolver();
+        assertThat(resolver.reverseResolve("127.0.0.1")).isEqualTo("localhost");
+        assertThat(resolver.reverseResolve("127.0.0.2")).isEqualTo("localhost2");
+        assertThat(resolver.reverseResolve("127.0.0.20")).isEqualTo("localhost20");
+        assertThat(resolver.reverseResolve("localhost20")).isEqualTo("localhost20");
+    }
+}
